c7abe4c7c2d2d76409745f3acc608f4e8b550e33
David Beveridge <davidjbeveridge@gmail.com>
2014-09-22 10:12:33 -0600

    extract validators

--- 01/lib/user.js
+++ 01/lib/user.js
@@ -1,14 +1,64 @@
+function attrAccessor(obj, attrName) {
+  var attrValue;
+  Object.defineProperty(obj, attrName, {
+    get: function(){
+      return attrValue;
+    },
+    set: function(value){
+      obj._dirty = true;
+      attrValue = value;
+    }
+  });
+}
+
+function attrAccessors(obj, attrNames) {
+  attrNames.forEach(function(attr){
+    attrAccessor(obj, attr);
+  });
+}
+
 function User(adapter) {
   this.adapter = adapter;
   this.save = this.save.bind(this);
+  this._dirty = false;
+
+  attrAccessors(this, ['name', 'email']);
 }
+
+User.validateFormat = function(attr, format) {
+  User.validators = User.validators || [];
+  User.validators.push(function(user) {
+    if(!format.test(user[attr])) return [attr, 'is not the correct format'];
+  });
+};
+
+User.validatePresence = function(attr) {
+  User.validators = User.validators || [];
+  User.validators.push(function(user) {
+    if(!user[attr]) return [attr, 'required'];
+  });
+};
+
+User.validatePresence('name');
+User.validatePresence('email');
+User.validateFormat('email', /.+@.+\..+/);
 
 User.prototype = {
   save: function() {
-    if(!this.name) throw new Error('name required');
-    if(!this.email) throw new Error('email required');
-    if(!/.+@.+\..+/.test(this.email)) throw new Error('email is not the correct format');
+    this.validate();
     this.adapter.save(this);
+    this._dirty = false;
+  },
+
+  dirty: function() {
+    return this._dirty;
+  },
+
+  validate: function() {
+    User.validators.forEach(function(validator){
+      var err = validator(this);
+      if(err) throw new Error(err.join(' '));
+    }, this);
   }
 };
 
--- 01/spec/user_spec.js
+++ 01/spec/user_spec.js
@@ -3,7 +3,7 @@ require('./spec_helper.js');
 var User = require('../lib/user');
 
 describe('User', function(){
-  describe('save', function(){
+  describe('#save', function(){
 
     var user, adapter;
 
@@ -13,11 +13,13 @@ describe('User', function(){
     });
 
     it('errors if name is not set', function(){
+      user.name = "";
       expect(user.save).to.throw(/name required/);
     });
 
     it('errors if email is not set', function() {
       user.name = "John Smith";
+      user.email = "";
       expect(user.save).to.throw(/email required/);
     });
 
@@ -32,6 +34,40 @@ describe('User', function(){
       user.email = "john.smith@example.com";
       user.save();
       expect(adapter.save).to.be.calledWith(user);
+    });
+
+    it('clears the dirty flag if all is well', function(){
+      user.name = "John Smith";
+      user.email = "john.smith@example.com";
+      user.save();
+      expect(user.dirty()).to.equal(false);
+    });
+  });
+
+  describe('#dirty', function() {
+    var user, adapter;
+
+    beforeEach(function(){
+      adapter = { save: sinon.spy() };
+      user = new User(adapter);
+    });
+
+    it('returns true when name has changed', function(){
+      user.name = "John Smith";
+      expect(user.dirty()).to.equal(true);
+    });
+
+    it('returns false if name has not changed', function(){
+      expect(user.dirty()).to.equal(false);
+    });
+
+    it('returns true when email has changed', function(){
+      user.email = "john.smith@example.com";
+      expect(user.dirty()).to.equal(true);
+    });
+
+    it('returns false if email has not changed', function(){
+      expect(user.dirty()).to.equal(false);
     });
   });
 });
